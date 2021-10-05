b1d2431adb857ff04c639c7e18d0f06f8ae5286b
vinicius-hrq <vinicius19h@gmail.com>
2021-10-05 11:24:50 -0300

    Ajustando estrutura de arquivos

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
