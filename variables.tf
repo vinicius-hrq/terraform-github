5f747926fbc45b48ab9e34d28f61868efedf732f
vinicius-hrq <vinicius19h@gmail.com>
2021-10-05 09:56:05 -0300

    Criacao de repositorio

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "github_token" {
+    type = string
+    description = "A GitHub OAuth / Personal Access Token."
+}
+
+variable "github_owner" {
+    type = string
+    description = "This is the target GitHub organization or individual user account to manage."
+}
